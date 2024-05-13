b1c5716cbbf7b4a2c45df262d6e9c1a7d9b0501f
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-13 09:00:40 +0200

    :sparkles: connect and add JavaScript code to change the footer year

--- 09-Omnifood-Optimization/js/script.js
+++ 09-Omnifood-Optimization/js/script.js
@@ -1,20 +1,42 @@
+/* 
+LEARNING BASIC JAVASCRIPT!!!
+
+console.log('Hello me!');
+
+const myName = 'Nora Nordmann';
+console.log('Hello ' + myName);
+
+const h1 = document.querySelector('.heading-primary');
+console.log(h1);
+
+h1.addEventListener('click', () => {
+  h1.textContent = myName;
+  h1.style.backgroundColor = 'red';
+  h1.style.padding = '5rem';
+}); 
+*/
+
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
 
